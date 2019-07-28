e83c74153e7eccd69d3aca7370530e98cfa436ce
Oliver Wright <oli.wright@blueyonder.co.uk>
2019-07-28 16:42:37 -0700

    styled share page

--- client/src/pages/Share/Share.js
+++ client/src/pages/Share/Share.js
@@ -2,30 +2,18 @@ import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles";
 import Grid from "@material-ui/core/Grid";
-
+import { Container } from "@material-ui/core";
 import ShareItemForm from "../../components/ShareItemForm";
 import ShareItemPreview from "../../components/ShareItemPreview";
 
-/*
-  TODO: Create ShareItemFrom and ShareItemPreview in the components dir
-  and call them from this file.
-
-  ShareItemForm is the form that our User will use to add a new item
-
-  When the user is filling ShareItemForm, we will show a preview of
-  this item using the ShareItemPreview.
-  Hint: It should look like any other Item card.
-
-*/
-
 const Share = ({ classes, tags }) => {
   return (
-    <div>
-      <Grid container spacing={4}>
-        <Grid item>
+    <div className={classes.root}>
+      <Grid container justify="center">
+        <Grid item className={classes.preview}>
           <ShareItemPreview />
         </Grid>
-        <Grid item>
+        <Grid item className={classes.inputForm}>
           <ShareItemForm tags={tags} />
         </Grid>
       </Grid>
--- client/src/pages/Share/styles.js
+++ client/src/pages/Share/styles.js
@@ -1,24 +1,17 @@
 const styles = theme => ({
   root: {
-    flexGrow: 1,
-    height: "100%",
-    background: theme.palette.primary.main,
-    padding: theme.spacing(5),
-    [theme.breakpoints.up("md")]: {
-      padding: theme.spacing(20)
-    }
+    width: "100vw",
+    paddingTop: "10vh"
   },
-  headline: {
-    fontWeight: 500,
-    color: theme.palette.text.primary,
-    fontSize: theme.typography.h3.fontSize,
-    [theme.breakpoints.up("md")]: {
-      fontSize: theme.typography.h1.fontSize
-    }
+  preview: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "50%"
   },
-  subheading: {
-    fontWeight: 400,
-    color: "white"
+  inputForm: {
+    display: "flex",
+    width: "50%"
   }
 });
 export default styles;
